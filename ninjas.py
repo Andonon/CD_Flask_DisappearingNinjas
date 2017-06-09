423c78dec43cc26e83874f1e53250c1c0343cda3
Troy Center <troycenter1@gmail.com>
2017-06-09 11:58:15 -0700

    updated ninjas.py with all static routes

---
+++ ninjas.py
@@ -0,0 +1,41 @@
+'''Coding Dojo Assignment, Flask Fundamentals, "Disappearing Ninjas"
+    by Troy Center June 2017, troycenter1@gmail.com
+'''
+#pylint: disable=C0103
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    '''Home page'''
+    pass
+
+@app.route('/ninja')
+def ninjapage():
+    '''Ninja page'''
+    pass
+
+@app.route('/blue')
+def bluepage(): 
+    '''Blue ninja page'''
+    pass
+
+@app.route('/orange')
+def orangepage(): 
+    '''Orange ninja page'''
+    pass
+
+@app.route('/red')
+def redpage():
+    '''Red ninja page'''
+    pass
+
+@app.route('/purple')
+def purplepage():
+    '''Purple ninja page'''
+
+@app.route('/<invalidcolor>')
+def invalidcolorpage(): 
+    '''Invalid Color Selection page''' 
+    pass
